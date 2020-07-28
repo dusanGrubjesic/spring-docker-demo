a6e85f8915f684fc1738a891b8c501e77d95c995
Dusan Grubjesic <grubjesic.dusan@gmail.com>
2020-07-28 14:29:53 +0200

    git init

---
+++ src/test/java/com/sample/code/simpledemo/SimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.code.simpledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
